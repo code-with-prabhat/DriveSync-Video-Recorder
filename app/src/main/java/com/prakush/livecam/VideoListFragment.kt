1f5fc3303c82864a2bdcdaa29c1e583c84849bf7
Prabhat Kushwah <prabhatkushwah012@gmail.com>
2026-05-25 17:58:20 +0530

    added fragments

--- app/src/main/java/com/prakush/livecam/VideoListFragment.kt
+++ app/src/main/java/com/prakush/livecam/VideoListFragment.kt
@@ -5,15 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.prakush.livecam.databinding.FragmentFirstBinding
+import com.prakush.livecam.databinding.FragmentVideoListBinding
 
 class VideoListFragment : Fragment() {
+
+    private var _binding: FragmentVideoListBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_video_list, container, false)
-    }
+        _binding = FragmentVideoListBinding.inflate(inflater, container, false)
+        return binding.root    }
 
 }
